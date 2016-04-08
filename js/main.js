715dc8346ed954ccb01e120797b4bddad0c38c82
DizzyDez <desc0137@algonquinlive.com>
2016-04-07 21:53:11 -0400

    thats as far as I could get.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var $body = $('body');
+var $svg = $('svg');
 
 var $btn = $('.btn');
 var $engage = $('.engage');
 var $lines = $('.lines');
 
-$btn.on('click', function () {
+$btn.on('click', function (e) {
   $lines.toggleClass('draw');
+  $lines.toggleClass('lines');
+  $svg.toggleClass('lined');
   $engage.toggleClass('orange');
 });
